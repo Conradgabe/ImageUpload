d06fe5241a929ea8c3ae7035103d91850a4f66fc
Conradgabe <gisuekebho5880@gmail.com>
2023-02-15 16:04:38 +0000

    static configuration

--- imageupLoader/settings.py
+++ imageupLoader/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
 
     # Local app
     'core.apps.CoreConfig',
+
+    # Third party app
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'C:/Users/Hp/Desktop/Image_uploader/static/'
-STATIC_ROOT = 'static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 MEDIA_ROOT = '/media/'
